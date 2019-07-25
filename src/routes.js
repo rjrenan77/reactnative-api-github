5081904a26c49ee937f003e21d325fdc831f8fff
Renan Soares <rjrenan77@gmail.com>
2019-07-24 21:14:15 -0300

    projeto reactnative finalizado

---
+++ src/routes.js
@@ -0,0 +1,25 @@
+import { createAppContainer, createStackNavigator } from 'react-navigation';
+
+import Main from './Pages/Main';
+import User from './Pages/User';
+
+const Routes = createAppContainer(
+  createStackNavigator(
+    {
+      Main,
+      User,
+    },
+    {
+      headerLayoutPreset: 'center',
+      headerBackTitleVisible: false,
+      defaultNavigationOptions: {
+        headerStyle: {
+          backgroundColor: '#7159c1',
+        },
+        headerTintColor: '#FFF',
+      },
+    }
+  )
+);
+
+export default Routes;
